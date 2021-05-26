781fb6095eaaa0ec78b043e21a3e010d67e42b78
Stephen Brady <stephenbrady365@gmail.com>
2021-05-26 22:13:17 +0100

    Bubble Sort and tests

---
+++ src/bubbleSort.js
@@ -0,0 +1,26 @@
+// Time complexity => O(n*n)
+// Sorts in place so returning the array is unnecessary
+
+const bubbleSort = (arr) => {
+  if (arr.length < 2) return;
+
+  let lastIndex = arr.length - 1;
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (i + 1 > lastIndex) break;
+
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        sorted = false;
+      }
+    }
+    lastIndex--;
+  }
+};
+
+module.exports = bubbleSort;
