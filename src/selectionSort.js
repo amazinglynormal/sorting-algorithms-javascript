e24b25293c62220e688224ab8c6602c8e1cfaf55
Stephen <stephenbrady365@gmail.com>
2021-05-27 13:49:47 +0100

    Add selection sort and tests

---
+++ src/selectionSort.js
@@ -0,0 +1,21 @@
+// Time complexity => O(n^2)
+// Sorts in place so returning the array is unnecessary
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let lowestValueIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowestValueIndex]) {
+        lowestValueIndex = j;
+      }
+    }
+
+    if (lowestValueIndex != i) {
+      let temp = arr[i];
+      arr[i] = arr[lowestValueIndex];
+      arr[lowestValueIndex] = temp;
+    }
+  }
+};
+
+module.exports = selectionSort;
